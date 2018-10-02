e6f621825ef5a37b8657eb65fde0676f0bda4eae
carlocadiz <carlocadiz71@gmail.com>
2018-10-02 21:24:17 +0000

    added new url form and set up post request

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -10,6 +12,14 @@ var urlDatabase = {
 };
 
 //console.log(urlDatabase."b2Vn2");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:id", (req, res) => {
   var index = req.params.id
